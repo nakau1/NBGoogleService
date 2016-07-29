278444a6727da3bfda4f15bb1b392457e9bd35b5
中安佑一 <yuichi.nakayasu@gmail.com>
2016-07-29 12:45:17 +0900

    CreateLibrary

---
+++ NBGoogleService.podspec
@@ -0,0 +1,9 @@
+d::Spec.new do |s|
+  s.name         = "NBGoogleService"
+  s.version      = "1.0.0"
+  s.summary      = "NeroBlu: Wrapped Google Services"
+  s.description  = ""
+  s.frameworks   = "Foundation", "UIKit"
+  s.source       = { :git => "https://github.com/nakau1/NBGoogleService" }
+  s.source_files = "NBGoogleService/*"
+end
---
+++ NBGoogleService/NBGoogleService.h
@@ -0,0 +1,19 @@
+//
+//  NBGoogleService.h
+//  NBGoogleService
+//
+//  Created by 中安佑一 on 2016/07/29.
+//  Copyright © 2016年 yuichi.nakayasu@gmail.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NBGoogleService.
+FOUNDATION_EXPORT double NBGoogleServiceVersionNumber;
+
+//! Project version string for NBGoogleService.
+FOUNDATION_EXPORT const unsigned char NBGoogleServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NBGoogleService/PublicHeader.h>
+
+
